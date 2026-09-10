e94761c9d92bba521fc6063bd094947700b40fd9
kongmajitenshi <dksk3504@gmail.com>
2026-09-10 21:17:57 +0900

    260910

--- Practice1/Assets/Bullet.cs
+++ Practice1/Assets/Bullet.cs
@@ -9,14 +9,16 @@ public class Bullet : MonoBehaviour
     {
         moveDirection = dir.normalized;  // 대각선 속도 보정
     }
-
-    private void Start()
-    {
-        Destroy(gameObject, 3f);  // 화면 밖으로 사라지고 3초 후 삭제
-    }
+    
 
     void Update()
     {
         transform.position += (Vector3)moveDirection * speed * Time.deltaTime;
     }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    
 }
--- Practice1/Assets/BulletSpawner.cs
+++ Practice1/Assets/BulletSpawner.cs
@@ -2,22 +2,50 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Transform playerTransform;
-    [SerializeField] private float spawnInterval = 0.5f;
+    [SerializeField] private GameObject bulletPrefab;  // 총알 원본 설계도(청사진)
+    [SerializeField] private Transform playerTransform;  // 조준할 플레이어 위치
+    [SerializeField] private float spawnInterval = 0.5f;  // 스폰 주기
 
     private float timer = 0f;
     private float spawnDistanceX = 10f;
     private float spawnDistanceY = 6f;
 
-
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            spawnBullet();
+            timer = 0f;
+        }
     }
+
+    void spawnBullet()
+    {
+        if (playerTransform == null) return;
+        Vector3 spawnPosition = Vector3.zero;
+        int side = Random.Range(0, 4);
+
+        switch (side)
+        {
+            case 0:
+                spawnPosition = new Vector3(Random.Range(-spawnDistanceX, spawnDistanceX),spawnDistanceY, 0);
+                break;
+            case 1:
+                spawnPosition = new Vector3(Random.Range(-spawnDistanceX, spawnDistanceX),-spawnDistanceY, 0);
+                break;
+            case 2:
+                spawnPosition = new Vector3(-spawnDistanceX, Random.Range(-spawnDistanceY, spawnDistanceY), 0);
+                break;
+            case 3:
+                spawnPosition = new Vector3(spawnDistanceX, Random.Range(-spawnDistanceY, spawnDistanceY), 0);
+                break;
+        }
+        
+        // 총알 생성
+        GameObject newBullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);
+        Vector2 dirctionToPlayer = playerTransform.position - spawnPosition;
+        newBullet.GetComponent<Bullet>().SetDirection(dirctionToPlayer);
+    }
+    
 }
